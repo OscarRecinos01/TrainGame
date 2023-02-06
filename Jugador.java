ca9206e5caa1dce84146fc2f6f964a62f4d775fe
Oscar Recinos <oscar.recinos01@telusinternational.com>
2023-02-06 11:41:36 -0600

    add jugador class

---
+++ Jugador.java
@@ -0,0 +1,40 @@
+package homework.code;
+
+public class Jugador{
+    private Tren tren;
+    private String nombre;
+    private int tiempoABatir;
+    
+    
+    public Jugador(String nombre, int tiempoABatir) {
+        this.nombre = nombre;
+        this.tiempoABatir = tiempoABatir;
+    }
+
+    public void avanzar(){
+        tren.avanzar();
+
+    }
+    public void frenar(){
+        tren.detenerse();
+        
+    }
+    public void pitar(){
+        tren.pitar();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getTiempoABatir() {
+        return tiempoABatir;
+    }
+
+    public Tren getTren() {
+        return tren;
+    }
+    
+    
+
+}
