363c54a4abfd603f01ee7c096dc51d95d4af7d72
Oscar Recinos <oscar.recinos01@telusinternational.com>
2023-02-08 17:58:25 -0600

    adding svg diagram

---
+++ Main.java
@@ -0,0 +1,10 @@
+package TrainGame;
+
+public class Main {
+    public static void main(String[] args) {
+        JuegoDelTren jugar = new JuegoDelTren();
+        jugar.setGame();
+
+        
+    }
+}
